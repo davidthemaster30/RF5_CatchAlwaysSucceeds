dec81c1506b7d6f407bddf762fa37b55a372651c
David Tremblay <davidthemaster30@hotmail.com>
2026-07-04 23:02:22 -0400

    Add git release to release script

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,20 @@
 rm -rf obj
 rm -rf bin
 
-dotnet build RF5_CatchAlwaysSucceeds.csproj -f net6.0 -c Release
+CSPROJ_PATH="RF5_CatchAlwaysSucceeds.csproj"          # path to your .csproj
 
-zip -j 'RF5_CatchAlwaysSucceeds_v1.1.1.zip' './bin/Release/net6.0/RF5_CatchAlwaysSucceeds.dll'
+dotnet build $CSPROJ_PATH -f net6.0 -c Release
+
+VERSION=$(grep -oP '(?<=<Version>)[^<]+' "$CSPROJ_PATH" || true)
+PROJECTNAME=$(grep -oP '(?<=<AssemblyName>)[^<]+' "$CSPROJ_PATH" || true)
+ZIP_NAME="${PROJECTNAME}_v${VERSION}.zip"
+
+zip -j "${ZIP_NAME}" './bin/Release/net6.0/RF5_CatchAlwaysSucceeds.dll'
+
+git tag "v${VERSION}"
+git push origin "v${VERSION}"
+ 
+gh release create "v${VERSION}" "${ZIP_NAME}" \
+  --title "v${VERSION}" \
+  --generate-notes
+  
